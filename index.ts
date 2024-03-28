0585fbcca7e1426bcd04ef2e7030d8294c49c9c1
Nimrh Fasih <nimrahfasih1@gmail.com>
2024-03-28 11:11:21 -0700

    pushing my calculator

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "FirstNumber" },
+  { message: "Enter Second Number", type: "number", name: "SecondNumber" },
+  {
+    Message: "choose any one of the operators",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+if (answer.operator === "Addition") {
+  console.log("Addition = " + (answer.FirstNumber + answer.SecondNumber));
+} else if (answer.operator === "Subtraction") {
+  console.log("Subtraction = " + (answer.FirstNumber - answer.SecondNumber));
+} else if (answer.operator === "Multiplication") {
+  console.log("Multiplication = " + answer.FirstNumber * answer.SecondNumber);
+} else if (answer.operator === "Division") {
+  console.log("Division = " + answer.FirstNumber / answer.SecondNumber);
+} else {
+  console.log("Please Select Valid Operator");
+}
